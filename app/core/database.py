b82797bbb555d474a80733f065313e9ec83b6b80
OXAYR <uzairmaqsood451@gmail.com>
2025-08-01 12:21:57 +0500

    initialize the model

--- app/core/database.py
+++ app/core/database.py
@@ -10,3 +10,10 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
+
+# Due to circular import issue writing the import here (Circular import issue is basically when two files try to import each other at the same time )
+
+from app.models.user import User  
+
+def init_db():
+    User.metadata.create_all(bind=engine)
